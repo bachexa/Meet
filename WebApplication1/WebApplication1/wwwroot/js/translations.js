3a3ec14ce4c22adde7f893b29617939ae7bf6080
BachoWebLpc\GtppLpc <bacho1988@gmail.com>
2025-08-01 14:27:21 +0400

    remove slider translations

--- WebApplication1/WebApplication1/wwwroot/js/translations.js
+++ WebApplication1/WebApplication1/wwwroot/js/translations.js
@@ -1,17 +1,6 @@
 ﻿// translations.js
 export const translations = {
     en: {
-        slider: {
-            title1: "Make friends with",
-            subtitle1: "Start meetings, share moments and work together easily!",
-            button1: "Download now",
-
-            title2: "Find a friend if they are free. Via:",
-            subtitle2: "Start Searching, share moments and work together easily!.",
-            button2: "Start now",
-
-            // და ასე შემდეგ...
-        },
         auth: {
             usernamePlaceholder: "Username or Email",
             passwordPlaceholder: "Password",
@@ -37,15 +26,6 @@ export const translations = {
         },
     },
     ka: {
-        slider: {
-            title1: "შეიძინე მეგობრები",
-            subtitle1: "დაიწყე შეხვედრები, გაუზიარე მომენტები და იმუშავე ერთად!",
-            button1: "ჩამოტვირთე ახლა",
-
-            title2: "იპოვე მეგობარი თუ თავისუფალია MeetDesk-ის მეშვეობით",
-            subtitle2: "დაიწყე ძიება და იმუშავე ეფექტურად.",
-            button2: "დაწყება",
-        },
         auth: {
             usernamePlaceholder: "მომხმარებელი ან ელფოსტა",
             passwordPlaceholder: "პაროლი",
