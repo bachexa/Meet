18184b43086d653e23987849c8475a7ba4a94161
NikaNoniashvili <nikanoniashvili30@gmail.com>
2025-12-17 18:08:06 +0400

    imagechange

--- WebApplication1/WebApplication1/Controllers/SliderController.cs
+++ WebApplication1/WebApplication1/Controllers/SliderController.cs
@@ -23,7 +23,7 @@ namespace WebApplication1.Controllers
             {
                 HeaderText = "Organize your  meetings via<br /><span style='font-weight:700;font-size:42px;color:#224541;'><span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "Start meetings, share moments and work together easily! Start meetings, share moments and work together easily! Start meetings, share moments and work together easily!",
-                Img = "/images/teams-hero.png",
+                Img = "/images/videocalls.png",
                 Language = "en",
                 SliderButton = "Download now"
             },
@@ -34,7 +34,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "Start meetings, share moments and work together easily!",
-                Img = "/images/teams-hero2.png",
+                Img = "/images/Fly.png",
                 Language = "en",
                 SliderButton = "Download now"
             },
@@ -45,7 +45,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "Start meetings, share moments and work together easily!",
-                Img = "/images/teams-hero3.png",
+                Img = "/images/Fly2.png",
                 Language = "en",
                 SliderButton = "Download now"
             },
@@ -56,7 +56,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "Start meetings, share moments and work together easily!",
-                Img = "/images/teams-hero4.png",
+                Img = "/images/videocall2.png",
                 Language = "en",
                 SliderButton = "Download now"
             },
@@ -65,7 +65,7 @@ namespace WebApplication1.Controllers
             {
                 HeaderText = "დაგეგმე შენი  შეხვედრები<br /><span style='font-weight:700;font-size:42px;color:#224541;'><span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "დაიწყე შეხვედრები, გააზიარე მომენტები და იმუშავე მარტივად ერთად!",
-                Img = "/images/teams-hero.png",
+                Img = "/images/videocalls.png",
                 Language = "ka",
                 SliderButton = "გადმოწერეთ"
             },
@@ -76,7 +76,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "დაიწყე შეხვედრები, გააზიარე მომენტები და იმუშავე მარტივად ერთად!",
-                Img = "/images/teams-hero2.png",
+                Img = "/images/FlyKa.png",
                 Language = "ka",
                 SliderButton = "გადმოწერეთ"
             },
@@ -87,7 +87,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "დაიწყე შეხვედრები, გააზიარე მომენტები და იმუშავე მარტივად ერთად!",
-                Img = "/images/teams-hero3.png",
+                Img = "/images/FlyKa2.png",
                 Language = "ka",
                 SliderButton = "გადმოწერეთ"
             },
@@ -98,7 +98,7 @@ namespace WebApplication1.Controllers
                               "<span style='font-weight:700;font-size:42px;color:#224541;'>" +
                               "<span style='color:#1f5c4e;'>Meet</span>Desk</span>",
                 ParagraphText = "დაიწყე შეხვედრები, გააზიარე მომენტები და იმუშავე მარტივად ერთად!",
-                Img = "/images/teams-hero4.png",
+                Img = "/images/videocall2.png",
                 Language = "ka",
                 SliderButton = "გადმოწერეთ"
             }};
