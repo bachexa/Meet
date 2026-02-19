d8865da9509de5c2fbf3115c1b7fba366ade49fc
gravitacia <gravitacia@users.noreply.github.com>
2026-02-19 21:42:25 +0400

    sssssss

--- WebApplication1/WebApplication1/Models/PlansAll.cs
+++ WebApplication1/WebApplication1/Models/PlansAll.cs
@@ -2,9 +2,9 @@
 {
     public class PlansAll
     {
-       // public string? Language { get; set; }
-       // public string? PlansTitle { get; set; }
-       // public string? PlansDescription { get; set; }
+        public string? Language { get; set; }
+        public string? PlansTitle { get; set; }
+        public string? PlansDescription { get; set; }
         public string? Img { get; set; }
         public string? MenuItem { get; set; }
         public List<PlansCard>? Plans { get; set; }
